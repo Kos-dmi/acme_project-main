834d577abe6ee714a89a42fab609921f0f9bb719
Dmitrii Kosteev <d_kosteev@mail.ru>
2025-04-16 01:10:08 +0300

    Add login form

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,10 +1,12 @@
-from .forms import BirthdayForm
-from .models import Birthday
-from .utils import calculate_birthday_countdown
+# birthday/views.py
 from django.views.generic import (
     CreateView, DeleteView, DetailView, ListView, UpdateView
 )
 from django.urls import reverse_lazy
+
+from .forms import BirthdayForm
+from .models import Birthday
+from .utils import calculate_birthday_countdown
 
 
 class BirthdayListView(ListView):
@@ -14,17 +16,17 @@ class BirthdayListView(ListView):
 
 
 class BirthdayCreateView(CreateView):
+    model = Birthday
     form_class = BirthdayForm
-    success_url = reverse_lazy('birthday:list')
 
 
 class BirthdayUpdateView(UpdateView):
+    model = Birthday
     form_class = BirthdayForm
-    success_url = reverse_lazy('birthday:list')
 
 
 class BirthdayDeleteView(DeleteView):
-    form_class = BirthdayForm
+    model = Birthday
     success_url = reverse_lazy('birthday:list')
 
 
@@ -32,12 +34,8 @@ class BirthdayDetailView(DetailView):
     model = Birthday
 
     def get_context_data(self, **kwargs):
-        # Получаем словарь контекста:
         context = super().get_context_data(**kwargs)
-        # Добавляем в словарь новый ключ:
         context['birthday_countdown'] = calculate_birthday_countdown(
-            # Дату рождения берём из объекта в словаре context:
             self.object.birthday
         )
-        # Возвращаем словарь контекста.
         return context

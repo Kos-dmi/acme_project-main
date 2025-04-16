50f6ae0195af039ea3170ad0e5ce64b4f990dcb6
Dmitrii Kosteev <d_kosteev@mail.ru>
2025-04-16 22:54:40 +0300

    Add user autentification

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -16,7 +16,7 @@ class BirthdayForm(forms.ModelForm):
         # Указываем модель, на основе которой должна строиться форма.
         model = Birthday
         # Указываем, что надо отобразить все поля.
-        fields = '__all__'
+        exclude = ('author',)
         widgets = {
             'birthday': forms.DateTimeInput(
                 format='%Y-%m-%d',
